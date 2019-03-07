60f0b990e82eb09520c3203edf204a51c124f85f
Andy Bell <andy.bell.github@gmail.com>
2019-03-07 10:29:56 +0000

    Break PATH out to it's own file.

---
+++ zsh/path.zsh
@@ -0,0 +1,8 @@
+# Add all Path alterations here...
+export PATH=/bin:$PATH
+export PATH=$HOME/bin:$PATH
+export PATH=/usr/local/bin:$PATH
+export PATH=/usr/local/share/npm/bin:$PATH
+export PATH=$HOME/.sr/tools/bin:$PATH
+export PATH=$HOME/.cabal/bin:$PATH
+export PATH=$HOME/.cargo/bin:$PATH
