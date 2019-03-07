5974b83cb14a5cfe671559daac070a453373dea3
Andy Bell <andy.bell.github@gmail.com>
2019-03-07 10:27:12 +0000

    Add in aliases from bash_profile and from my old zshrc

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,24 @@
 alias reload!='. ~/.zshrc'
+alias be='bundle exec'
+alias bi='bundle install'
+alias gpg='gpg2'
+alias gp='git push'
+alias gcm="git commit -m"
+alias retag='`brew --prefix`/bin/ctags -R --exclude=.svn --exclude=.git --exclude=log --exclude=tmp *'
+alias review="git push origin HEAD:refs/for/master"
+alias c="git commit"
+alias typeless='history | tail -n 20000 | sed "s/.*  //"  | sort | uniq -c | sort -g | tail -n 100'
+alias be="bundle exec"
+alias gau="git add -u"
+alias gca="git commit --amend"
+alias gcf="git commit --fixup HEAD"
+bind -x '"\C-l": clear; ls'
+alias t="bundle exec rspec && bundle exec cucumber"
+alias ber="bundle exec rspec"
+alias bec="bundle exec cucumber"
+alias u="cd .."
+alias e="vim ."
+alias fs="foreman start"
+alias ss="./script/server"
+alias fu="docker-compose kill && docker-compose up"
+
